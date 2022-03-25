6ab3cffce200e6876dd44a94fa7584768b29a330
ckpd3453 <98898121+ckpd3453@users.noreply.github.com>
2022-03-25 15:32:48 +0530

    Added UC-1 Problem to load the indian states Census information from a CSV file

---
+++ src/main/java/StateCensusAnalyser.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+public class StateCensusAnalyser {
+	private final String CSV_PATH = "C:\\Users\\SHREEKANT\\Desktop\\JavaPracticeProblems\\Census_Analyser_Problem\\CensusAnalyserProblem-Day29\\src\\main\\resources\\IndiaStateCensusData.csv";
+
+	public static void main(String[] args) throws IOException {
+
+		StateCensusAnalyser read = new StateCensusAnalyser();
+		read.readCSVData();
+
+	}
+
+	/**
+	 * create method readCSVData that reads the data from csv file
+	 * 
+	 * @throws IOException -throws exception
+	 */
+	public void readCSVData() throws IOException {
+
+		/**
+		 * taking try and Catch block to handle the catch exceptions
+		 */
+		List<StateCensusCSV> users = new ArrayList<StateCensusCSV>();
+		   String line = "";
+		   BufferedReader reader = new BufferedReader(new FileReader(CSV_PATH));
+		   reader.readLine();
+		   
+		   while((line = reader.readLine()) != null) {
+		    String[] fields = line.split(",");
+		    
+		    if(fields.length > 0) {
+		     StateCensusCSV user = new StateCensusCSV();
+		     user.setState(fields[0]);
+		     user.setPop(Integer.parseInt(fields[1]));
+		     user.setArea(Integer.parseInt(fields[2]));
+		     user.setDensity(Integer.parseInt(fields[3]));
+		     users.add(user);
+		    }
+		   }
+		   
+		   for(StateCensusCSV u: users) {
+		    System.out.printf("[state=%s, population=%d, area=%d, density=%d]\n", u.getState(),u.getPop(), u.getArea(), u.getDensity());
+		   }
+		   
+		  }
+}
---
+++ src/main/java/StateCensusCSV.java
@@ -0,0 +1,80 @@
+import com.opencsv.bean.CsvBindByName;
+
+public class StateCensusCSV {
+	/**
+	 * Seems that the CsvBindByName annotation always converts the column name to
+	 * UpperCase based on the OpenCSV source code Specifies a binding between a
+	 * column name of the CSV input and a field in a bean.
+	 */
+	@CsvBindByName(column = "State")
+	private String state;
+
+	@CsvBindByName(column = "Population")
+	private long pop;
+
+	@CsvBindByName(column = "AreaInSqKm")
+	private int area;
+
+	@CsvBindByName(column = "DensityPerSqKm")
+	private int density;
+	
+	//Generating Getters/Setters
+
+	public String getState() {
+		return state;
+	}
+
+
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+
+
+	public long getPop() {
+		return pop;
+	}
+
+
+
+	public void setPop(long pop) {
+		this.pop = pop;
+	}
+
+
+
+	public int getArea() {
+		return area;
+	}
+
+
+
+	public void setArea(int area) {
+		this.area = area;
+	}
+
+
+
+	public int getDensity() {
+		return density;
+	}
+
+
+
+	public void setDensity(int density) {
+		this.density = density;
+	}
+
+
+
+	/**
+	 * By overriding the toString() method of the Object class, we can return values
+	 * of the object, so we don't need to write much code.
+	 */
+	@Override
+	public String toString() {
+		return "IndiaStateCensus [ State = " + state + " , Population = " + pop + " , AreaInSqKm = " + area
+				+ " , density = " + density + " ]";
+	}
+}
